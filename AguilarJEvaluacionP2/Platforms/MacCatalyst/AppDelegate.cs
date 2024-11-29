ea004d0a1870a3baf2f9d49bd966d4269ff7a308
laboratorio <laboratorio@UPEAULA-31614>
2024-11-29 09:16:49 -0500

    Agregar archivos de proyecto.

---
+++ AguilarJEvaluacionP2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AguilarJEvaluacionP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
